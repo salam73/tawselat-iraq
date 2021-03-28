439e7b76d9bf8730327148689d528dd851d67810
Salam <salam73@gmail.com>
2021-03-28 17:33:27 +0200

    add test comment

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  //just test
 
   @override
   Widget build(BuildContext context) {
